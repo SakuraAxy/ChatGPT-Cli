42aaa414770cd8cf794ba4dc31e10257f10ab8c1
miaoxiang.wang <miaoxiang.wang@philips.com>
2023-04-14 15:31:24 +0800

    add include pkg

--- main.go
+++ main.go
@@ -3,51 +3,16 @@ package main
 import (
 	"bufio"
 	"context"
-	"errors"
 	"flag"
 	"fmt"
-	openai "github.com/sashabaranov/go-openai"
-	"io"
+
+	"github.com/SakuraAxy/ChatGPT-Cli/pkg/Chat"
+	"github.com/sashabaranov/go-openai"
+
 	"net/http"
 	"os"
 	"time"
 )
-
-func GetResponse(c *openai.Client, ctx context.Context, quesiton string) {
-	req := openai.ChatCompletionRequest{
-		Model:     openai.GPT3Dot5Turbo,
-		MaxTokens: 2000,
-		Messages: []openai.ChatCompletionMessage{
-			{
-				Role:    openai.ChatMessageRoleUser,
-				Content: quesiton,
-			},
-		},
-		Stream: true,
-	}
-
-	stream, err := c.CreateChatCompletionStream(ctx, req)
-	if err != nil {
-		fmt.Printf("ChatCompletionStream error: %v\n", err)
-		return
-	}
-	defer stream.Close()
-
-	fmt.Printf("Response: \n")
-	for {
-		response, err := stream.Recv()
-		if errors.Is(err, io.EOF) {
-			return
-		}
-
-		if err != nil {
-			fmt.Printf("\nStream error: %v\n", err)
-			return
-		}
-
-		fmt.Printf(response.Choices[0].Delta.Content)
-	}
-}
 
 func checkChatGPTConnect(timeOut time.Duration) error {
 	client := &http.Client{
@@ -114,7 +79,7 @@ func main() {
 		case "":
 			continue
 		default:
-			GetResponse(client, ctx, questions)
+			Chat.GetResponse(client, ctx, questions)
 		}
 
 	}
---
+++ pkg/Chat/Chat.go
@@ -0,0 +1,45 @@
+package Chat
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/sashabaranov/go-openai"
+	"io"
+)
+
+func GetResponse(c *openai.Client, ctx context.Context, quesiton string) {
+	req := openai.ChatCompletionRequest{
+		Model:     openai.GPT3Dot5Turbo,
+		MaxTokens: 2000,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: quesiton,
+			},
+		},
+		Stream: true,
+	}
+
+	stream, err := c.CreateChatCompletionStream(ctx, req)
+	if err != nil {
+		fmt.Printf("ChatCompletionStream error: %v\n", err)
+		return
+	}
+	defer stream.Close()
+
+	fmt.Printf("Response: \n")
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			return
+		}
+
+		if err != nil {
+			fmt.Printf("\nStream error: %v\n", err)
+			return
+		}
+
+		fmt.Printf(response.Choices[0].Delta.Content)
+	}
+}
